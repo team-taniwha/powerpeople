d0b87e470958154b12808fd19c19f43157668698
Nick Johnstone <ncwjohnstone@gmail.com>
2015-08-21 18:15:32 +1200

    Force https

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module People
   class Application < Rails::Application
+    config.force_ssl = true
+
     config.browserify_rails.commandline_options = "-t babelify"
 
     SimpleGoogleAuth.configure do |config|
