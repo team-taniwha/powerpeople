bc392ceeea9f03fe0c1177ebc4254651e4008cae
Nick Johnstone <ncwjohnstone@gmail.com>
2015-08-22 10:40:01 +1200

    Only force ssl in production, fix oauth in dev

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module People
   class Application < Rails::Application
-    config.force_ssl = true
-
     config.browserify_rails.commandline_options = "-t babelify"
 
     SimpleGoogleAuth.configure do |config|
